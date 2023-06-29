484b2f18e694f4ca5d2309877cceae8865e54aee
Jeremias Riquero <111074834+JereRiquero@users.noreply.github.com>
2023-06-29 19:39:53 -0300

    Add files via upload

---
+++ Clase7/logging_base.py
@@ -0,0 +1,21 @@
+"""
+Laboratorio III
+Clase 7
+Jeremias Riquero
+
+"""
+import logging as log
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('capa_datos.log'),
+                    log.StreamHandler()
+                ])
+if __name__ == '__main__':
+    log.debug('Mensaje de debug')
+    log.info('Mensaje de info')
+    log.warning('Mensaje de warning')
+    log.error('Mensaje de error')
+    log.critical('Mensaje de critical')
+
